a3cdde00ab48377e8d5fd0c1b178b63567681ed9
Shine <1333278+varioso@users.noreply.github.com>
2019-07-26 10:36:31 +0300

    Update steemConnectAPI.js

--- src/client/steemConnectAPI.js
+++ src/client/steemConnectAPI.js
@@ -2,7 +2,7 @@ import sc2 from 'sc2-sdk';
 
 const api = sc2.Initialize({
 app: "ftinetwork",
-baseURL: "https://steemconnect.com",
+baseURL: "https://api.steemconnect.com/api/oauth2/authorize",
 callbackURL: "http://network.futuretechinsider.com/callback",
 });
 
